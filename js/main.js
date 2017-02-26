0dad15f38d85e4cd345348c41059ca014e1a9cb8
pawell67 <pawellwankiewicz@gmail.com>
2017-02-26 15:24:55 +0100

    added delete button, modulo,

--- js/main.js
+++ js/main.js
@@ -2,10 +2,10 @@ var result = document.getElementById('result');
 var numbers = document.querySelectorAll('button.number');
 var numbersLength = numbers.length;
 
-var number, limit = 15, output, action, score;
+var number, limit = 15, action, score;
 var actions = document.querySelectorAll('button.action');
 var actionsLength = actions.length;
-
+var output = result.innerHTML;
 for (var i = 0; i < numbersLength; i++) {
 
     numbers[i].addEventListener('click', function () {
@@ -14,7 +14,7 @@ for (var i = 0; i < numbersLength; i++) {
 
         output = result.innerHTML += number;
         if (output.length > limit) {
-            alert("this nubmer is too long!")
+            alert("This nubmer is too long!")
         }
     }, false);
 }
@@ -34,3 +34,16 @@ for (i = 0; i < actionsLength; i++) {
     }, false);
 }
 
+var del =document.querySelector('.delete');
+del.addEventListener('click',function(){
+    result.innerHTML='';
+},false);
+
+var coma = document.querySelector('.coma');
+coma.addEventListener('click',function(){
+
+    if(result.innerHTML === "."){
+        result.innerHTML= "0.";
+
+    }
+},false);
